b260540be930245300142fc6fbafffb23257068a
samuel-wanyoike <125296142+samuel-wanyoike@users.noreply.github.com>
2023-05-07 18:30:13 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+
+import { Axios } from 'axios';
 import './App.css';
+import { useEffect, useState } from 'react';
+
+
 
 function App() {
+
+  const [name, setName] = useState('');
+  const [age, setAge] = useState({});
+
+  const fetchData = () => {
+    // Axios.get(`https://api.agify.io/?name=${name}`).then((res) => {
+    //   console.log(res.data);
+    // });
+
+    fetch(`https://api.agify.io/?name=${name}`)
+    .then(response => response.json())
+    .then(data => setAge(data));
+  };
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input placeholder='Ex. Samuel...' onChange={(event) => {
+        setName(event.target.value);
+      }}/>
+      <button onClick={fetchData}>Predict age</button>
+      <h1>name: {age.name} </h1>
+      <h1>count: {age.count} </h1>
+      <h1>Predicted age: {age.age} </h1>
     </div>
   );
 }
